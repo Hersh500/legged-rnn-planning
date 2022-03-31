a1b414126b60b2309236afab63c435c1c339e1ef
hersh500 <hersh500@gmail.com>
2022-03-31 14:07:32 -0400

    add heightmap.py

---
+++ heightmap.py
@@ -0,0 +1,125 @@
+import numpy as np
+import csv
+import hopper2d
+
+class HeightMap:
+    def __init__(self, terrain_array, heightmap_info):
+        self.info = heightmap_info
+        self.terrain_array = terrain_array
+        return
+
+
+    def at(self, x_m, y_m):
+        row, col = self.m_to_idx((x_m, y_m))
+        return self.terrain_array[row][col]
+
+
+    def normal_at(self, x_m, y_m):
+        return np.pi/2  
+
+
+    def plotSteps(self, step_sequence_m, lines = False):
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        labels = np.array([''] * self.terrain_array.size).reshape(self.terrain_array.shape)
+        for i, step in enumerate(step_sequence_m):
+            labels[self.m_to_idx(step)] = str(i)
+
+        if lines:
+            ax = sns.heatmap(self.terrain_array,
+                             cmap='inferno',
+                             annot=labels,
+                             annot_kws = {'fontsize':8},
+                             fmt='s',
+                             linewidths = 0.1, linecolor='black',square = True) 
+        else:
+            ax = sns.heatmap(self.terrain_array,
+                             cmap='inferno',
+                             annot=labels,
+                             annot_kws = {'fontsize':8},
+                             fmt='s', square = True) 
+            
+        plt.show()
+             
+
+    def plotStepsFromCSV(self, csv_fname, lines = False):
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+
+        step_sequence_m = []
+        with open(csv_fname) as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                step_sequence_m.append([float(row[0]), float(row[1])])
+
+        labels = np.array([''] * self.terrain_array.size).reshape(self.terrain_array.shape)
+        for i, step in enumerate(step_sequence_m):
+            labels[self.m_to_idx(step)] = str(i)
+
+        if lines:
+            ax = sns.heatmap(self.terrain_array,
+                             cmap='inferno',
+                             annot=labels,
+                             annot_kws = {'fontsize':8},
+                             fmt='s',
+                             linewidths = 0.1, linecolor='black',square = True) 
+        else:
+            ax = sns.heatmap(self.terrain_array,
+                             cmap='inferno',
+                             annot=labels,
+                             annot_kws = {'fontsize':8},
+                             fmt='s', square = True) 
+            
+        plt.show()
+
+
+    def m_to_idx(self, step):
+        x_m, y_m = step
+        row_idx = np.clip(int((y_m - self.info["corner_val_m"][1])/self.info["disc"]),
+                          0, self.terrain_array.shape[0])
+        col_idx = np.clip(int((x_m - self.info["corner_val_m"][0])/self.info["disc"]),
+                          0, self.terrain_array.shape[1])
+        return row_idx, col_idx
+
+
+    def save(self, fname):
+        hopper2d.saveTerrainAsCSV(fname, self.terrain_array, self.info["corner_val_m"],
+                                  self.info["disc"], self.info["friction"])
+
+
+def HeightMapFromCSV(fname):
+    terrain_array = []
+    flag = False
+    with open(fname, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if flag:
+               disc, friction, corner_x, corner_y = [float(h) for h in row]
+            elif row[0] == "D":
+                flag = True 
+            else:
+                tmp = [float(h) for h in row]    
+                terrain_array.append(tmp)
+    terrain_array = np.array(terrain_array)
+    heightmap_info = {"disc": disc, "corner_val_m": [corner_x, corner_y],
+                      "max_x": terrain_array.shape[1] * disc + corner_x,
+                      "max_y": terrain_array.shape[0] * disc + corner_y,
+                      "friction": friction}
+    return HeightMap(terrain_array, heightmap_info)
+        
+
+def randomDitchHeightMap(max_x, max_y, disc, num_ditches):
+    x0 = 0
+    y0 = -2
+    terrain_array, terrain_func = hopper2d.generateRandomTerrain2D(max_x - x0, max_y - y0, disc, num_ditches) 
+    return HeightMap(terrain_array, (x0, y0), disc)
+
+
+def gapHeightMap(heightmap_info, gap_xs, gap_widths):
+    x0 = heightmap_info["corner_val_m"][0]
+    y0 = heightmap_info["corner_val_m"][1]
+
+    gap_info =  [[gap_xs[i] - x0, 0, gap_widths[i], heightmap_info["max_y"] - y0] for i in range(len(gap_xs))]
+    terrain_array, terrain_func = hopper2d.generateTerrain2D(heightmap_info["max_x"] - x0, heightmap_info["max_y"] - y0,
+                                                             heightmap_info["disc"], gap_info)
+    return HeightMap(terrain_array, heightmap_info)
